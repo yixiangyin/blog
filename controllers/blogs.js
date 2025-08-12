ff9e4642d98397865b97811b7db868255f96fd07
yixiangyin <y1027020239@qq.com>
2025-08-12 13:36:49 +1000

    models

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,15 +1,5 @@
-const mongoose = require("mongoose")
-
 const blogsRouter = require("express").Router()
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-})
-
-const Blog = mongoose.model("Blog", blogSchema)
+const Blog = require("../models/blog")
 
 blogsRouter.get("/", (request, response) => {
   Blog.find({}).then((blogs) => {
