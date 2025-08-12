80d9b977dbeee98f17d754a29efc36abeef34bb4
yixiangyin <y1027020239@qq.com>
2025-08-12 12:20:09 +1000

    4.2 utils

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-require('dotenv').config()
-const express = require('express')
-const mongoose = require('mongoose')
+require("dotenv").config()
+const express = require("express")
+const mongoose = require("mongoose")
+const logger = require("./utils/logger")
 
 const app = express()
 
@@ -11,20 +12,20 @@ const blogSchema = mongoose.Schema({
   likes: Number,
 })
 
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = mongoose.model("Blog", blogSchema)
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl)
 
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
+app.get("/api/blogs", (request, response) => {
   Blog.find({}).then((blogs) => {
     response.json(blogs)
   })
 })
 
-app.post('/api/blogs', (request, response) => {
+app.post("/api/blogs", (request, response) => {
   const blog = new Blog(request.body)
 
   blog.save().then((result) => {
@@ -34,5 +35,5 @@ app.post('/api/blogs', (request, response) => {
 
 const PORT = 3003
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  logger.info(`Server running on port ${PORT}`)
 })
