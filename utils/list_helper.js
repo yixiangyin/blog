44abb9d300e647adb40470c5090451ecba5d2ca6
yixiangyin <y1027020239@qq.com>
2025-08-16 23:19:51 +1000

    4.5 favorite blog

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -6,6 +6,12 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0)
 }
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return null
+
+  return blogs.reduce((fav, blog) => blog.likes > fav.likes ? blog : fav)
+}
+
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
