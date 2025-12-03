2b8d916cec7888e55104aa54a17510722db6df4f
yixiangyin <y1027020239@qq.com>
2025-12-04 07:46:13 +1100

    4.15

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const mongoose = require("mongoose")
 const logger = require("./utils/logger")
 const config = require("./utils/config")
 const blogsRouter = require("./controllers/blogs")
+const usersRouter = require('./controllers/users')
+const middleware = require('./utils/middleware')
 
 const app = express()
 
@@ -18,7 +20,12 @@ mongoose
   })
 
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use("/api/blogs", blogsRouter)
+app.use("/api/users", usersRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
--- models/blog.js
+++ models/blog.js
@@ -4,6 +4,7 @@ const blogSchema = mongoose.Schema({
   author: String,
   url: String,
   likes: { type: Number, default: 0 },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
 })
 
 blogSchema.set("toJSON", {
