3f6115ebcfbb2fa3938f75b4e0fcb0aa60596d4d
yixiangyin <y1027020239@qq.com>
2025-12-07 10:35:47 +1100

    4.18&19 token based auth

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const logger = require("./utils/logger")
 const config = require("./utils/config")
 const blogsRouter = require("./controllers/blogs")
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 
 const app = express()
@@ -24,6 +25,7 @@ app.use(middleware.requestLogger)
 
 app.use("/api/blogs", blogsRouter)
 app.use("/api/users", usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,22 +1,34 @@
 const blogsRouter = require("express").Router()
 const Blog = require("../models/blog")
 const User = require("../models/user")
+const jwt = require("jsonwebtoken")
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization")
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "")
+  }
+  return null
+}
 
 blogsRouter.get("/", async (request, response) => {
-  const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
+  const blogs = await Blog.find({}).populate("user", { username: 1, name: 1 })
   response.json(blogs)
 })
 
 blogsRouter.post("/", async (request, response) => {
   const body = request.body
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "token invalid" })
+  }
+  const user = await User.findById(decodedToken.id)
 
+  if (!user) {
+    return response.status(400).json({ error: "UserId missing or not valid" })
+  }
   if (!body.title || !body.url) {
     return response.status(400).json({ error: "title or url missing" })
-  }
-  
-  const user = await User.findOne({})
-  if (!user) {
-    return response.status(400).json({ error: 'no users in database' })
   }
 
   const blog = new Blog({
