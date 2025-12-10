f58384f7126b8982ba4f5a6fea9cab922171d0ab
yixiangyin <y1027020239@qq.com>
2025-12-11 07:18:01 +1100

    clean up for app.js

--- app.js
+++ app.js
@@ -1,24 +1,15 @@
+require("express-async-errors")
 const express = require("express")
 const mongoose = require("mongoose")
-const logger = require("./utils/logger")
-const config = require("./utils/config")
 const blogsRouter = require("./controllers/blogs")
-const usersRouter = require('./controllers/users')
-const loginRouter = require('./controllers/login')
-const middleware = require('./utils/middleware')
+const usersRouter = require("./controllers/users")
+const loginRouter = require("./controllers/login")
+const config = require("./utils/config")
+const middleware = require("./utils/middleware")
 
 const app = express()
 
-logger.info("connecting to", config.MONGODB_URI)
-
-mongoose
-  .connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info("connected to MongoDB")
-  })
-  .catch((error) => {
-    logger.error("error connection to MongoDB:", error.message)
-  })
+mongoose.connect(config.MONGODB_URI)
 
 app.use(express.json())
 app.use(middleware.requestLogger)
@@ -26,7 +17,7 @@ app.use(middleware.tokenExtractor)
 
 app.use("/api/blogs", blogsRouter)
 app.use("/api/users", usersRouter)
-app.use('/api/login', loginRouter)
+app.use("/api/login", loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
